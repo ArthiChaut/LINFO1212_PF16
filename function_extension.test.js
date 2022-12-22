9cbbafbf32a84dc1d0245c2e643310ef1ba5c3a8
Velkiz <maxime.munier@student.uclouvain.be>
2022-12-22 19:23:18 +0100

    update test fonctionnel

--- function_extension.test.js
+++ function_extension.test.js
@@ -1,12 +1,19 @@
+const { User, Clothes } = require("./models");
+const bcrypt = require('bcryptjs');
+const { Op } = require("sequelize");
 const function_extension = require('./function_extension')
 
-describe('Check if account already created'),() => {
-    test('Account already exists',()=>{
-        expect(function_extension.accountExist('velkiz').toBe(true))
-        expect(function_extension.accountExist('max@gmail.com').toBe(true))
-        })
-    test('Account doesnt exist yet',()=>{
-        expect(function_extension.accountExist('polo21').toBe(false))
-        expect(function_extension.accountExist('poloman@outlook.com').toBe(false))
+describe("Check if account already exists in database", () => {
+    test("account exists", async () => {
+        const resultUsername = await function_extension.accountExist('velkiz')
+        const resultEmail = await function_extension.accountExist('velkiz')
+     expect(resultUsername).toBe(true);
+     expect(resultEmail).toBe(true);
+    });
+    test("account doesnt exist", async () => {
+        const resultUsername = await function_extension.accountExist('natshara')
+        const resultEmail = await function_extension.accountExist('natshara')
+     expect(resultUsername).toBe(false);
+     expect(resultEmail).toBe(false);
     })
-}
+})
